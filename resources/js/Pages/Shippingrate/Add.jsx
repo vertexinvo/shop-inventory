5642862d809c7fe528a04004480bac5a2e063fdf
vertexinvo123 <sufyank.vertexinvo@gmail.com>
2024-12-05 21:14:01 +0500

    update

--- resources/js/Pages/Shippingrate/Add.jsx
+++ resources/js/Pages/Shippingrate/Add.jsx
@@ -1,81 +1,178 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik'
-import React from 'react'
-import { FaWallet , FaEdit} from 'react-icons/fa'
-import { MdDelete } from 'react-icons/md';
-import { GiTwoCoins } from 'react-icons/gi';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import React from 'react';
 import { Head, router } from '@inertiajs/react';
 import * as Yup from 'yup';
- 
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+
 export default function List(props) {
-  const { auth } = props
+  const { auth } = props;
+
   return (
-      <AuthenticatedLayout
-        tax={auth.tax}
-          header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Add Tax</h2>}
-      >
-          <Head title="Tax" />
+    <AuthenticatedLayout
+      tax={auth.tax}
+      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Add Shipping Rate</h2>}
+    >
+      <Head title="Shipping Rate" />
 
-          <div className="flex flex-col px-4  mt-10 mx-auto w-full">
-          <div className="w-full ">
-            
-          <div class="font-sans antialiased bg-grey-lightest">
+      <div className="flex flex-col px-4 mt-10 mx-auto w-full">
+        <div className="w-full">
+          <div className="font-sans antialiased bg-grey-lightest">
+            <div className="w-full bg-grey-lightest">
+              <div className="container mx-auto py-3 px-5">
+                <div className="w-full lg:w-full mx-auto bg-white rounded shadow">
+                  <Formik
+                    enableReinitialize
+                    initialValues={{
+                      area_name: '',
+                      postal_code: '',
+                      city_name: 'Karachi',  
+                      state_name: 'Sindh',   
+                      country_name: 'Pakistan', 
+                      fee: '',
+                    }}
+                    validationSchema={Yup.object({
+                      area_name: Yup.string().required('Area name is required'),
+                      city_name: Yup.string().required('City name is required'),
+                      state_name: Yup.string().required('State name is required'),
+                      country_name: Yup.string().required('Country name is required'),
+                      fee: Yup.string().required('Fee is required'),
+                    })}
+                    onSubmit={(values, { setSubmitting, resetForm }) => {
+                      router.post(route('shippingrate.store'), values, { onSuccess: () => resetForm() });
+                    }}
+                  >
+                    <Form>
+                      <div className="py-4 px-8">
+                        <div className="flex mb-4">
+                          <div className="w-1/2 mr-1">
+                            <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="area_name">Area Name</label>
+                            <Field
+                              name="area_name"
+                              className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                              id="area_name"
+                              type="text"
+                              placeholder="Enter area name"
+                            />
+                            <ErrorMessage name="area_name" component="div" className="text-red-500 text-xs mt-1" />
+                          </div>
+                          <div className="w-1/2 ml-1">
+                            <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="postal_code">Postal Code</label>
+                            <Field
+                              name="postal_code"
+                              className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                              id="postal_code"
+                              type="text"
+                              placeholder="Enter postal code"
+                            />
+                            <ErrorMessage name="postal_code" component="div" className="text-red-500 text-xs mt-1" />
+                          </div>
+                        </div>
 
+                        <div className="flex mb-4">
+                          <div className="w-1/2 mr-1">
+                            {/* Set default value to 'Karachi' and hide the field */}
+                            {/* <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="city_name">
+                              City Name
+                            </label> */}
+                            <Field
+                              name="city_name"
+                              value="Karachi" // Set default value to Karachi
+                              className="hidden appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                              id="city_name"
+                              type="text"
+                              placeholder="Enter city name"
+                              disabled
+                            />
+                            <ErrorMessage
+                              name="city_name"
+                              component="div"
+                              className="text-red-500 text-xs mt-1"
+                            />
+                          </div>
 
-  <div class="w-full bg-grey-lightest">
-    <div class="container mx-auto py-3 px-5">
-      <div class="w-full lg:w-full mx-auto bg-white rounded shadow">
-          <Formik  enableReinitialize initialValues={{ name: '', cost: ''}}
-          validationSchema={Yup.object({
-            name: Yup.string().required('Name is required'),
-            cost: Yup.string().required('Cost is required'),           
-          })}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            router.post(route('tax.store'), values, { onSuccess: () => resetForm() });
-          }}
-          >
-            <Form>
+                          <div className="w-1/2 ml-1">
+                            {/* Set default value to 'Sindh' and hide the field */}
+                            {/* <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="state_name">
+                              State Name
+                            </label> */}
+                            <Field
+                              name="state_name"
+                              value="Sindh" // Set default value to Sindh
+                              className="hidden appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                              id="state_name"
+                              type="text"
+                              placeholder="Enter state name"
+                              disabled
+                            />
+                            <ErrorMessage
+                              name="state_name"
+                              component="div"
+                              className="text-red-500 text-xs mt-1"
+                            />
+                          </div>
+                        </div>
 
-          
-            <div class="py-4 px-8">
-                <div class="flex mb-4">
-                    <div class="w-1/2 mr-1">
-                        <label class="block text-grey-darker text-sm font-bold mb-2" for="first_name">Name</label>
-                        <Field name="name" class="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="name" type="text" placeholder="Enter name"/>
-                        <ErrorMessage name="name" component="div" className="text-red-500 text-xs mt-1" />
-                    </div>
-                    <div class="w-1/2 mr-1">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="cost">Cost</label>
-                    <Field name="cost"  step="0.01" class="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="cost" type="number" placeholder="Enter tax cost"/>
-                    <ErrorMessage name="cost" component="div" className="text-red-500 text-xs mt-1" />
+                        <div className="flex mb-4">
+                        <div className="w-1/2 ml-1">
+                            <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="fee">Fee</label>
+                            <Field
+                              name="fee"
+                              // 
+                              className="appearance-none border rounded w-full py-2 px-3 text-grey-darker number"
+                              id="fee"
+                              type="number"
+                              placeholder="Enter fee"
+                            />
+                            <ErrorMessage name="fee" component="div" className="text-red-500 text-xs mt-1" />
+                          </div>
+                          <div className="w-1/2 mr-1">
+                            {/* Set default value to 'Pakistan' and hide the field */}
+                            {/* <label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="country_name">
+                              Country Name
+                            </label> */}
+                            <Field
+                              name="country_name"
+                              value="Pakistan" // Set default value to Pakistan
+                              className="hidden appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+                              id="country_name"
+                              type="text"
+                              placeholder="Enter country name"
+                              disabled
+                            />
+                            <ErrorMessage
+                              name="country_name"
+                              component="div"
+                              className="text-red-500 text-xs mt-1"
+                            />
+                          </div>
+
+                          
+                        </div>
+
+                        <div className="flex items-center justify-start gap-1 mt-8">
+                          <button
+                            className="bg-black hover:bg-blue-dark text-white font-bold py-2 px-4 rounded-lg"
+                            type="submit"
+                          >
+                            Submit
+                          </button>
+                          <button
+                            onClick={() => router.get(route('shippingrate.index'))}
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg"
+                            type="button"
+                          >
+                            Close
+                          </button>
+                        </div>
+                      </div>
+                    </Form>
+                  </Formik>
                 </div>
-                </div>
-              
-                
-                
-                <div class="flex items-center justify-start gap-1 mt-8">
-                    <button class="bg-black hover:bg-blue-dark text-white font-bold py-2 px-4 rounded-lg" type="submit">
-                        Submit
-                    </button>
-                    <button  onClick={() => router.get(route('tax.index'))} class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg" type="button">
-                        Close
-                    </button>
-                </div>
+              </div>
             </div>
-          </Form>
-          </Formik>
-        </div>
-      
-    </div>
-  </div>
-</div>
-
-           
           </div>
         </div>
-
-         
-      </AuthenticatedLayout>
+      </div>
+    </AuthenticatedLayout>
   );
 }
-
