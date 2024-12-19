1a660322247eac1667054193ac16e18e264a8259
Danish <167245031+danishSolvix@users.noreply.github.com>
2024-12-19 22:55:42 +0500

    build

--- resources/js/Pages/Order/List.jsx
+++ resources/js/Pages/Order/List.jsx
@@ -120,7 +120,7 @@ console.log(orders)
                 <option value="">Select Status</option>
                 <option value="pending">Pending</option>
                 <option value="completed">Completed</option>
-                <option value="cancelled">Cancelled</option>
+                <option value="cancel">Cancelled</option>
               </select>
  
               {selectId.length > 0 && (
