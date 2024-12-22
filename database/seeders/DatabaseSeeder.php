0dab49de901db88b829e6ef208eebfce28fd6348
Huzaifa <vertexinvo@gmail.com>
2024-12-22 18:18:57 +0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Setting;
 use App\Models\ShippingRate;
 use Illuminate\Database\Seeder;
 
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PermissionSeeder::class,
             ShippingRateSeeder::class,
+            SettingSeeder::class,
         ]);
     }
 }
