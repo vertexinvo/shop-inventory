13be51e89988f0539645e51e209f08fbfe5c4369
Huzaifa <vertexinvo@gmail.com>
2024-12-24 12:23:43 +0500

    Update DomainDatabaseSwitcher.php

--- app/Http/Middleware/DomainDatabaseSwitcher.php
+++ app/Http/Middleware/DomainDatabaseSwitcher.php
@@ -31,21 +31,25 @@ class DomainDatabaseSwitcher
                 'database' => env('DB_SHOPA_DATABASE'),
                 'username' => env('DB_SHOPA_USERNAME'),
                 'password' => env('DB_SHOPA_PASSWORD'),
+                'host' => env('DB_SHOPA_HOST'),
             ],
             'localhost:8001' => [
                 'database' => env('DB_SHOPB_DATABASE'),
                 'username' => env('DB_SHOPB_USERNAME'),
                 'password' => env('DB_SHOPB_PASSWORD'),
+                'host' => env('DB_SHOPB_HOST'),
             ],
             'celltech.vertexinvo.io' => [
                 'database' => env('DB_CELLTECH_DATABASE'),
                 'username' => env('DB_CELLTECH_USERNAME'),
                 'password' => env('DB_CELLTECH_PASSWORD'),
+                'host' => env('DB_CELLTECH_HOST'),
             ],
             'inventorysystem.vertexinvo.io' => [
                 'database' => env('DB_VERTEXINVO_DATABASE'),
                 'username' => env('DB_VERTEXINVO_USERNAME'),
                 'password' => env('DB_VERTEXINVO_PASSWORD'),
+                'host' => env('DB_VERTEXINVO_HOST'),
             ],
         ];
    
@@ -54,6 +58,7 @@ class DomainDatabaseSwitcher
             Config::set('database.connections.mysql.database', $domainToDatabase[$domain]['database']);
             Config::set('database.connections.mysql.username', $domainToDatabase[$domain]['username']);
             Config::set('database.connections.mysql.password', $domainToDatabase[$domain]['password']);
+            Config::set('database.connections.mysql.host', $domainToDatabase[$domain]['host']);
         
             DB::purge('mysql');
             DB::reconnect('mysql');
