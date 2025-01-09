a34891aed2d1a06392f67b4a0fd12690e4686256
Huzaifa <vertexinvo@gmail.com>
2025-01-09 17:06:43 +0500

    Create 2025_01_09_115432_add_cols_code_in_products_and_orders_table.php

---
+++ database/migrations/2025_01_09_115432_add_cols_code_in_products_and_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('code')->nullable();
+        });
+        Schema::table('items', function (Blueprint $table) {
+            $table->string('code')->nullable();
+        });
+
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('code')->nullable();
+        });
+        Schema::table('exchangeitems', function (Blueprint $table) {
+            $table->string('code')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products_and_orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
