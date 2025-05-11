9ce105bd776f45245909849e8bf02bd8a3eec832
muhammad shah <muhammadshah4589@gmail.com>
2025-05-11 15:50:22 +0500

    changes in create

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -16,6 +16,7 @@ import { GoGraph } from "react-icons/go";
 import { GiExpense } from 'react-icons/gi';
 import { IoMdMenu } from 'react-icons/io';
 import LinkDeviceQrcode from '@/Components/LinkDeviceQrcode';
+import { FaPlusCircle } from 'react-icons/fa';
 
 
 export default function AuthenticatedLayout({ header, headerTitle, children }) {
@@ -75,54 +76,79 @@ export default function AuthenticatedLayout({ header, headerTitle, children }) {
                                 </NavLink>
                             </div> */}
                             </div>
-
                             <div className="hidden sm:ms-6 sm:flex sm:items-center">
-                                <div className="relative ms-3">
-                                    <Dropdown>
-                                        <Dropdown.Trigger>
-                                            <span className="inline-flex rounded-md">
-                                                <button
-                                                    type="button"
-                                                    className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
-                                                >
-                                                    {/* <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo" /> */}
+  <div className="flex items-baseline justify-end relative ms-3">
+    {/* Create New Dropdown */}
+    <div className="flex items-center gap-2">
+      <Dropdown>
+        <Dropdown.Trigger>
+          <span className="inline-flex rounded-md">
+            <button
+              type="button"
+              className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+            >
+              create new
+              <FaPlusCircle className="ms-2 h-4 w-4" />
+            </button>
+          </span>
+        </Dropdown.Trigger>
+        <Dropdown.Content>
+          <div className="px-4 py-2 text-sm font-semibold text-gray-800">
+            Create
+          </div>
+          <hr className="border-gray-200" />
+          <Dropdown.Link href={route('customer.create')}>
+            Create Customer
+          </Dropdown.Link>
+          <Dropdown.Link href={route('order.create')}>
+            Create Order
+          </Dropdown.Link>
+          <Dropdown.Link href={route('product.create')}>
+            Create Product
+          </Dropdown.Link>
+        </Dropdown.Content>
+      </Dropdown>
+    </div>
 
-                                                    {user.name}
-
-                                                    <svg
-                                                        className="-me-0.5 ms-2 h-4 w-4"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 20 20"
-                                                        fill="currentColor"
-                                                    >
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                            clipRule="evenodd"
-                                                        />
-                                                    </svg>
-                                                </button>
-                                            </span>
-                                        </Dropdown.Trigger>
-
-                                        <Dropdown.Content>
-                                            <Dropdown.Link
-                                                href={route('profile.edit')}
-                                            >
-                                                Profile
-                                            </Dropdown.Link>
-                                            <Dropdown.Link
-                                                href={route('logout')}
-                                                method="post"
-                                                as="button"
-                                            >
-                                                Log Out
-                                            </Dropdown.Link>
-                                        </Dropdown.Content>
-                                    </Dropdown>
-                                </div>
-                            </div>
-
+    {/* User Dropdown */}
+    <Dropdown>
+      <Dropdown.Trigger>
+        <span className="inline-flex rounded-md">
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+          >
+            {user.name}
+            <svg
+              className="-me-0.5 ms-2 h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </span>
+      </Dropdown.Trigger>
+      <Dropdown.Content>
+        <Dropdown.Link href={route('profile.edit')}>
+          Profile
+        </Dropdown.Link>
+        <Dropdown.Link
+          href={route('logout')}
+          method="post"
+          as="button"
+        >
+          Log Out
+        </Dropdown.Link>
+      </Dropdown.Content>
+    </Dropdown>
+  </div>
+</div>
                             <div className="-me-2 flex items-center sm:hidden">
                                 <button
                                     onClick={() =>
